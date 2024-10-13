088d453c92c869b95476158aeee3f4955efb0fa3
raviavaiya3 <21bmiit007@gmail.com>
2024-10-13 16:53:27 +0530

    Steamlit

---
+++ Steamlit/steamlit_basics.py
@@ -0,0 +1,8 @@
+import steamlit_basics as sb
+
+sb.title('Hello, Devlopers')
+sb.header('Welcome to Streamlit Basics')
+sb.subheader('This is a subheader')
+sb.write('This is a paragraph of text')
+sb.code('print("Hello, World!")')
+Name=sb.text_input('Enter Your Name :')
