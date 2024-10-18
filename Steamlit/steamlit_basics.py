748f3271fc17b6d4b0f3af16da3c25bd23eca4ae
raviavaiya3 <21bmiit007@gmail.com>
2024-10-18 17:02:37 +0530

    Dataset Add

--- Steamlit/steamlit_basics.py
+++ Steamlit/steamlit_basics.py
@@ -3,9 +3,12 @@ import streamlit as st
 st.title('Hello, Devlopers')
 st.header('Welcome to Streamlit Basics')
 st.subheader('This is a subheader')
-st.write('This is a paragraph of text')
+st.write('## This is a paragraph of text')
 st.code('print("Hello, World!")')
 Name=st.text_input('Enter Your Name :')
+Date=st.date_input('Selectde  Date :')
+
 
 if st.button('Click Me!'):
     st.write('Welcome, ' + Name + '!')
+    st.write('Selected Date :',Date)
